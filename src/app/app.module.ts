b785bb321918523c826959fd72111de21a57c0ae
NoteEdu <eduardo.tsilva@hotmail.com>
2021-06-04 16:16:19 -0300

    123. Formulário de contatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContatoComponent } from './contato/contato.component';
-import { ContatoService } from './contato.service';
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatInputModule } from "@angular/material/input";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ContatoComponent } from "./contato/contato.component";
+import { ContatoService } from "./contato.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContatoComponent
-  ],
+  declarations: [AppComponent, ContatoComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
   ],
   providers: [ContatoService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
