539baaaf433ec44773ce93569bd7382678c69a63
NoteEdu <eduardo.tsilva@hotmail.com>
2021-06-04 23:06:42 -0300

    128. Melhorias no layout de contatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatInputModule } from "@angular/material/input";
 import { ReactiveFormsModule } from "@angular/forms";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatTableModule } from "@angular/material/table";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -22,6 +27,10 @@ import { ContatoService } from "./contato.service";
     MatInputModule,
     HttpClientModule,
     ReactiveFormsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
   ],
   providers: [ContatoService],
   bootstrap: [AppComponent],
